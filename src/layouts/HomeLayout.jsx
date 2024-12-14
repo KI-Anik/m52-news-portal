02577abab6b4b4f9c7506e0b4256fc8314fd421f
KI-Anik <iamkhairul101@gmail.com>
2024-12-14 21:31:52 +0600

    left nav category map done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-component/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -16,8 +17,12 @@ const HomeLayout = () => {
                 <Navbar></Navbar>
             </nav>
 
-            <main className="">
-
+            <main className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-3">
+        <aside className=" left col-span-3">
+            <LeftNavbar></LeftNavbar>
+        </aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right Navbar</aside>
             </main>
         </div>
     );
