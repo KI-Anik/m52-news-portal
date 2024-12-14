3fb271105aaece0233f61557cf8625be16c834db
KI-Anik <iamkhairul101@gmail.com>
2024-12-14 21:57:53 +0600

    right nav design done

---
+++ src/components/layout-component/RightNavbar.jsx
@@ -0,0 +1,13 @@
+import FindUs from "../FindUs";
+import SocialLogin from "../SocialLogin";
+
+const RightNavbar = () => {
+    return (
+        <div className="space-y-5">
+           <SocialLogin/>
+           <FindUs/>
+        </div>
+    );
+};
+
+export default RightNavbar;
